3fa424794b93f6a44ae452fbe77868644109ce7d
sang thai <thaibasang8@gmail.com>
2019-12-11 16:50:53 +0700

    lan hai

--- app/src/main/java/com/example/quanlyoto_doan/Service/APIServices.java
+++ app/src/main/java/com/example/quanlyoto_doan/Service/APIServices.java
@@ -1,7 +1,7 @@
 package com.example.quanlyoto_doan.Service;
 
 public class APIServices {
-    private static String baseurl="http://192.168.1.26/cuahangquanlyoto/public/";
+    private static String baseurl="http://192.168.1.34/cuahangquanlyoto/public/";
 
     public static DataService getService(){
         return APIRetrofitClient.getClient(baseurl).create(DataService.class);
